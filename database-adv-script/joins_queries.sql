17f2b725391df325bfd3182f37fba9e069fe3366
Olumide <adeoyeolumbah@gmail.com>
2025-09-01 23:39:31 +0100

    Ordered the LEFT JOIN by the property

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -14,7 +14,8 @@ FROM
     property p
 LEFT JOIN 
     reviews r ON p.property_id = r.property_id
-
+ORDER BY 
+    p.property_id;
 
 -- Write a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
 SELECT  
@@ -22,4 +23,4 @@ SELECT
 FROM 
     user u
 OUTER JOIN 
-    booking b ON u.user_id = b.user_id 
+    booking b ON u.user_id = b.user_id;
